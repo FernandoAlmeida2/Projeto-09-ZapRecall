68ca9931c7f2990d8dfecff93f3d4c4a6c64898e
Fernando Almeida <fernandoalmeida@fisica.ufc.br>
2022-09-27 07:29:48 -0300

    Conteúdo inicial do projeto

---
+++ src/App.js
@@ -0,0 +1,9 @@
+export default function App(){
+    return(
+        <div className = "pai-de-todos">
+            <header></header>
+            <main></main>
+            <footer></footer>
+        </div>
+    )
+}
